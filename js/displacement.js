710b80d4d4ad16d6426f5ad6523f2ad056d5b749
ahsb <fkm-2009@hotmail.com>
2018-10-17 22:47:38 +0300

    the begin of ahsb v2

--- js/displacement.js
+++ js/displacement.js
@@ -8,43 +8,61 @@ function calculate() {
 	if (document.getElementById('side1').checked) {
 		if(!displacement) {
 			displacement = displacementfinal - displacementfirst ;
+			if (isNaN(displacement)) displacement = displacement || 0;
+			if (isFinite(displacement)) displacement = displacement || 0;
 			document.getElementById("D").value=show(displacement) + "    m";
 		} else if(!displacementfirst) {
 			displacementfirst = displacementfinal - displacement ;
+			if (isNaN(displacementfirst)) displacementfirst = displacementfirst || 0;
+			if (isFinite(displacementfirst)) displacementfirst = displacementfirst || 0;
 			document.getElementById("Di").value=show(displacementfirst)  + "    m";
 		} else {
 			displacementfinal = displacement + displacementfirst ;
+			if (isNaN(displacementfinal)) displacementfinal = displacementfinal || 0;
+			if (isFinite(displacementfinal)) displacementfinal = displacementfinal || 0;
 			document.getElementById("Df").value=show(displacementfinal) + "    m";
 		}
 	}
 	if (document.getElementById('side2').checked) {
 		if(!displacement) {
 			displacement = displacementfinal - displacementfirst ;
+			if (isNaN(displacement)) displacement = displacement || 0;
+			if (isFinite(displacement)) displacement = displacement || 0;
 			document.getElementById("D").value=show(displacement) + "    m";
 		} else if(!displacementfirst) {
 			displacementfirst = displacementfinal - displacement ;
+			if (isNaN(displacementfirst)) displacementfirst = displacementfirst || 0;
+			if (isFinite(displacementfirst)) displacementfirst = displacementfirst || 0;
 			document.getElementById("Di").value=show(displacementfirst)  + "    m";
 		} else {
 			displacementfinal = displacementfirst + displacement ;
+			if (isNaN(displacementfinal)) displacementfinal = displacementfinal || 0;
+			if (isFinite(displacementfinal)) displacementfinal = displacementfinal || 0;
 			document.getElementById("Df").value=show(displacementfinal) + "    m";
 		}
 	}
 	if (document.getElementById('side3').checked) {
-	
+
 		if(!a) {
 			a =  Math.sqrt(r - b) ;
+			if (isNaN(a)) a = a || 0;
+			if (isFinite(a)) a = a || 0;
 			document.getElementById("Di").value=show(a);
 		}
 		if(!b) {
 			b =  Math.sqrt(r - a);
+			if (isNaN(b)) b = b || 0;
+			if (isFinite(b)) b = b || 0;
 			document.getElementById("Df").value=show(b);
 		}
 		if(!r) {
 			r =  Math.sqrt(a + b) ;
+			if (isNaN(r)) r = r || 0;
+			if (isFinite(r)) r = r || 0;
 			document.getElementById("D").value=show(r);}
 		}
 	}
-function show (answer){ 
+function show (answer){
 	return answer ;
 
 
--- js/voltage.js
+++ js/voltage.js
@@ -4,18 +4,26 @@ function calculate() {
 	var volts=parseFloat(document.getElementById("V").value.replace(",","."));
 	if(!ambers) {
 		ambers = volts / ohms ;
+		if (isNaN(ambers)) ambers = ambers || 0;
+		if (isFinite(ambers)) ambers = ambers || 0;
 		document.getElementById("I").value=dividing(ambers) + "    A";
-	} else if(!ohms) {
+	}
+	 if(!ohms) {
 		ohms = volts / ambers ;
+		if (isNaN(ohms)) ohms = ohms || 0;
+		if (isFinite(ohms)) ohms = ohms || 0;
 		document.getElementById("R").value=dividing(ohms)  + "    Ω";
-	} else {
+	}
+	if (!volts) {
 		volts = ambers * ohms ;
+		if (isNaN(volts)) volts = volts || 0;
+		if (isFinite(volts)) volts = volts || 0;
 		document.getElementById("V").value=multiply(volts) + "    V";
 	}
 }
 function dividing(answer) {
-	return answer
+	return answer;
 }
 function multiply(answer) {
-	return answer
+	return answer;
 }
